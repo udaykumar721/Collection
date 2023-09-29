fa1523920e1dd6dc180ffcf5faf64903a1990b77
udaykumar721 <uday56367@gmail.com.com>
2023-09-29 08:26:09 +0530

    [UDAY]add : Example Of List Generic

---
+++ CollectionDemo/CollectionDemo/ListDemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo
+{
+    internal class ListDemo
+    {
+        public void Lists()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            //List<string> listString = new List<string>();
+            //listString.Add("Uday");
+            //listString.Add("jaswanth");
+            //listString.Add("Sudheer");
+            //listString.Add("Manoj");
+            //listString.Add("amit");
+            //foreach (string s in listString)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            List<double> listDouble = new List<double>();
+            listDouble.Add(721.23);
+            listDouble.Add(724.34);
+            listDouble.Add(092.67);
+            listDouble.Add(696.78);
+            foreach (double d in listDouble)
+            {
+                Console.WriteLine(d);
+            }
+            Console.WriteLine("---------------");
+            listDouble.Reverse();
+            { 
+                foreach (double d in listDouble)
+                {
+                    Console.WriteLine(d);
+                }
+            }
+            Console.WriteLine(listDouble.Count);
+        }
+    }
+}
---
+++ CollectionDemo/CollectionDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDemo listDemo = new ListDemo();
+            listDemo.Lists();
+            Console.ReadLine();
+        }
+    }
+}
